8c7ea0e645f2c730c37cda28dd2c9d5bfeee9fdf
yibnl <yassin.kammoun@outlook.com>
2019-03-13 14:02:45 +0100

    Add internal xml's abstract syntax tree

---
+++ src/main/scala/dotty/xml/interpolator/Tree.scala
@@ -0,0 +1,27 @@
+package dotty.xml.interpolator
+
+import scala.util.parsing.input._
+
+object Tree {
+
+  sealed abstract class Node extends Positional
+  final case class Group(nodes: Seq[Node]) extends Node
+  final case class Elem(name: String, attributes: Seq[Attribute], empty: Boolean, children: Seq[Node]) extends Node {
+    def prefix: String = name.take(prefixEnd)
+    def label:  String = name.drop(prefixEnd + 1)
+    private def prefixEnd = name.indexOf(':')
+  }
+  final case class Text(text: String) extends Node
+  final case class Comment(text: String) extends Node
+  final case class Placeholder(id: Int) extends Node
+  final case class PCData(data: String) extends Node
+  final case class ProcInstr(target: String, proctext: String) extends Node
+  final case class EntityRef(name: String) extends Node
+  final case class Unparsed(data: String) extends Node
+  final case class Attribute(name: String, value: Seq[Node]) extends Positional {
+    def prefix: String = name.take(prefixEnd) 
+    def key:    String = name.drop(prefixEnd + 1)
+    def isNamespace = name.startsWith("xmlns")
+    private def prefixEnd = name.indexOf(':')
+  }
+}
