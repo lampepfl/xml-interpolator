d322e8be5d3f64195542a090307fa396700d491f
yibnl <yassin.kammoun@outlook.com>
2019-03-26 11:53:53 +0100

    Join the interpolation pieces together

--- src/main/scala/dotty/xml/interpolator/Interpolator.scala
+++ src/main/scala/dotty/xml/interpolator/Interpolator.scala
@@ -6,13 +6,22 @@ import scala.language.implicitConversions
 import scala.quoted.Exprs.LiftedExpr
 import scala.quoted.Toolbox.Default._
 
-object Interpolator extends MacroStringInterpolator[String] {
+object Interpolator extends MacroStringInterpolator[scala.xml.Node | scala.xml.NodeBuffer] {
 
   class StringContextOps(strCtx: => StringContext) {
-    inline def xml(args: Any*): String = ~{Interpolator('(strCtx), '(args))}
+    inline def xml(args: Any*): scala.xml.Node | scala.xml.NodeBuffer = ~{Interpolator('(strCtx), '(args))}
   }
   implicit inline def SCOps(strCtx: => StringContext): StringContextOps = new StringContextOps(strCtx)
 
-  protected def interpolate(strCtx: StringContext, args: List[Expr[Any]])(implicit reflect: Reflection): Expr[String] =
-    '{(~{strCtx.toExpr}).s(~{args.toExprOfList}: _*)}
+  protected def interpolate(strCtx: StringContext, args: List[Expr[Any]])(implicit reflect: Reflection): Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
+    val encoded = EncodeHole(strCtx)
+    val parser  = new Parser()
+    val parsed  = parser.parseAll(parser.XmlExpr, encoded) match {
+      case parser.Success(result, _) => result
+      case failed : parser.NoSuccess => throw new QuoteError(failed.msg)
+    }
+    ValidateAttribute(parsed)
+    TypeCheck(parsed, args)
+    Lift(parsed, args)
+  }
 }
