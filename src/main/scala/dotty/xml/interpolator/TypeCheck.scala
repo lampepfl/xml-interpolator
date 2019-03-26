36e9e2663c1af45cc2532ba3b229a89a09ade538
yibnl <yassin.kammoun@outlook.com>
2019-03-26 11:43:49 +0100

    Make placeholder type checking behave like scalac

--- src/main/scala/dotty/xml/interpolator/TypeCheck.scala
+++ src/main/scala/dotty/xml/interpolator/TypeCheck.scala
@@ -6,41 +6,30 @@ import scala.tasty._
 
 import Tree._
 
-class TypeCheck {
+object TypeCheck {
 
-  def apply(nodes: Seq[Node], args: List[Expr[Any]])(implicit reflection: Reflection): Seq[Node] = {
-    typecheck(nodes, args)
-    nodes
-  }
-
-  private def typecheck(nodes: Seq[Node], args: List[Expr[Any]])(implicit reflection: Reflection): Unit = {
-    nodes.foreach(typecheck(_, args))
-  }
-
-  private def typecheck(node: Node, args: List[Expr[Any]])(implicit reflection: Reflection): Unit = {
-    node match {
-      case Elem(name, attributes, empty, children) => attributes.foreach(typecheck(_, args))
-      case _ =>
-    }
-  }
-
-  private def typecheck(attribute: Attribute, args: List[Expr[Any]])(implicit reflection: Reflection): Unit = {
+  def apply(nodes: Seq[Node], args: List[Expr[Any]])(implicit reflection: Reflection): Unit = {
     import reflection._
-    // TODO
-    //  namespace => String
-    // !namespace => String, Seq[scala.xml.Node], Option[Seq[scala.xml.Node]]
-    attribute.value.foreach {
-      case Placeholder(id) =>
-        val term = args(id).unseal
-        if (!(term.tpe <:< definitions.StringType)) {
-          throw new QuoteError(
-            s"""type mismatch;
-              | found   : ${term.tpe}
-              | required: ${definitions.StringType}
-            """.stripMargin
-          )
-        }
+    nodes.foreach(node => node match {
+      case e : Elem =>
+        e.attributes.foreach(attribute => attribute.value match {
+          case Seq(Placeholder(id)) =>
+            val term = args(id).unseal
+            val expected = attribute.isNamespace match {
+              case true => Seq('[String].unseal.tpe)
+              case _ => Seq('[String].unseal.tpe,'[Seq[scala.xml.Node]].unseal.tpe, '[Option[Seq[scala.xml.Node]]].unseal.tpe)
+            }
+            if (!expected.exists(term.tpe <:< _)) {
+              throw new QuoteError(
+                s"""type mismatch;
+                  | found   : ${term.tpe}
+                  | required: ${expected.mkString(" | ")}
+                """.stripMargin
+              )
+            }
+          case _ =>
+        })
       case _ =>
-    }
+    })
   }
 }
