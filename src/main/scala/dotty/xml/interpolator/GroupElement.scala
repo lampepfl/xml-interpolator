7718e59343c1c608283bf9c02d150d0434e9cbd3
yibnl <yassin.kammoun@outlook.com>
2019-03-30 16:57:28 +0100

    Group elements enclosed by <xml:group> tags

---
+++ src/main/scala/dotty/xml/interpolator/GroupElement.scala
@@ -0,0 +1,18 @@
+package dotty.xml.interpolator
+
+import scala.quoted._
+
+import Tree._
+
+object GroupElement {
+
+  def apply(nodes: Seq[Node]): Seq[Node] = {
+    nodes.map(node => node match {
+      case elem : Elem =>
+        val children = apply(elem.children)
+        if (elem.name == "xml:group" && !elem.empty) Group(elem.children).setPos(elem.pos)
+        else elem.copy(children = children)
+      case _ => node
+    })
+  }
+}
