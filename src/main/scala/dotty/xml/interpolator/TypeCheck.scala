5e9a521aba4c2bf215f3f2ce81f3ebe6a3995ac0
yibnl <yassin.kammoun@outlook.com>
2019-04-16 13:44:34 +0200

    Use internal error reporter

--- src/main/scala/dotty/xml/interpolator/TypeCheck.scala
+++ src/main/scala/dotty/xml/interpolator/TypeCheck.scala
@@ -7,26 +7,27 @@ import dotty.xml.interpolator.Tree._
 
 object TypeCheck {
 
-  def apply(nodes: Seq[Node], args: List[Expr[Any]])(implicit reflect: Reflection): Unit = {
+  def apply(nodes: Seq[Node], args: List[Expr[Any]])(implicit reflect: Reflection, reporter: Reporter): Unit = {
     import reflect._
-    nodes.foreach(node => node match {
+    nodes.foreach {
       case elem : Elem =>
-        elem.attributes.foreach(attribute => attribute.value match {
-          case Seq(Placeholder(id)) =>
-            val term = args(id).unseal
-            val expected = attribute.isNamespace match {
-              case true => Seq('[String].unseal.tpe)
-              case _ => Seq('[String].unseal.tpe,'[Seq[scala.xml.Node]].unseal.tpe, '[Option[Seq[scala.xml.Node]]].unseal.tpe)
-            }
-            if (!expected.exists(term.tpe <:< _)) {
-              error(s"""type mismatch;
-                  | found   : ${term.tpe.widen.show}
-                  | required: ${expected.map(_.show).mkString(" | ")}
-                """.stripMargin, term.pos)
-            }
-          case _ =>
+        elem.attributes.foreach(attribute =>
+          attribute.value match {
+            case Seq(Placeholder(id)) =>
+              val term = args(id).unseal
+              val expected = attribute.isNamespace match {
+                case true => Seq('[String].unseal.tpe)
+                case _ => Seq('[String].unseal.tpe,'[Seq[scala.xml.Node]].unseal.tpe, '[Option[Seq[scala.xml.Node]]].unseal.tpe)
+              }
+              if (!expected.exists(term.tpe <:< _)) {
+                reporter.error(s"""type mismatch;
+                    | found   : ${term.tpe.widen.show}
+                    | required: ${expected.map(_.show).mkString(" | ")}
+                  """.stripMargin, term.seal)
+              }
+            case _ =>
         })
       case _ =>
-    })
+    }
   }
 }
