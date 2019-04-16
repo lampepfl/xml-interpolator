f95fee0a5035091a418aa6a95ca0d11857f4e589
yibnl <yassin.kammoun@outlook.com>
2019-04-16 13:43:38 +0200

    Clean up

--- src/main/scala/dotty/xml/interpolator/GroupElement.scala
+++ src/main/scala/dotty/xml/interpolator/GroupElement.scala
@@ -5,12 +5,12 @@ import dotty.xml.interpolator.Tree._
 object GroupElement {
 
   def apply(nodes: Seq[Node]): Seq[Node] = {
-    nodes.map(node => node match {
+    nodes.map {
       case elem : Elem =>
         val children = apply(elem.children)
         if (elem.name == "xml:group" && !elem.empty) Group(elem.children).setPos(elem.pos)
         else elem.copy(children = children)
-      case _ => node
-    })
+      case node => node
+    }
   }
 }
