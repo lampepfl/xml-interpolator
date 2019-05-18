171bd373a62bb697b77c568340fb76f5fd361b84
yibnl <yassin.kammoun@outlook.com>
2019-05-18 16:41:31 +0200

    Add tests on scope inheritance

--- src/test/scala/dotty/xml/interpolator/NamespaceTest.scala
+++ src/test/scala/dotty/xml/interpolator/NamespaceTest.scala
@@ -25,7 +25,29 @@ class NamespaceTest {
     assert(xml"""<foo xmlns:pre="a"><bar xmlns:pre="b"/></foo>""" ≈ <foo xmlns:pre="a"><bar xmlns:pre="b"/></foo>)
   }
 
-  @Test def scope(): Unit = {
+  @Test def scope1(): Unit = {
     assert(xml"""<foo xmlns:pre="scope0">${xml"<bar/>"}</foo>""" ≈ <foo xmlns:pre="scope0">{<bar/>}</foo>)
+  }
+
+  @Test def scope2(): Unit = {
+    assert(xml"""<foo xmlns:pre="scope0">${xml"<bar/>"}</foo>""" ≈ xml"""<foo xmlns:pre="scope0"><bar/></foo>""")
+  }
+
+  @Test def scope3(): Unit = {
+    assert(xml"""<foo xmlns:pre="scope0">${xml"<bar><baz/></bar>"}</foo>""" ≈ <foo xmlns:pre="scope0">{<bar><baz/></bar>}</foo>)
+  }
+
+  @Test def scope4(): Unit = {
+    assert(xml"""<foo xmlns:pre="scope0">${xml"""<bar>${xml"<baz/>"}</bar>"""}</foo>""" ≈ <foo xmlns:pre="scope0">{<bar>{<baz/>}</bar>}</foo>)
+  }
+
+  @Test def scope5(): Unit = {
+    assert(xml"""<foo xmlns:zero="scope0">${xml"""<bar xmlns:one="scope1">${xml"""<baz xmlns:two="scope2"/>"""}</bar>"""}</foo>""" ≈ <foo xmlns:zero="scope0">{<bar xmlns:one="scope1">{<baz xmlns:two="scope2"/>}</bar>}</foo>)
+  }
+
+  @Test def scope6(): Unit = {
+    val xml1 = xml"<bar/>"
+    val xml2 = <bar/>
+    assert(xml"""<foo xmlns:pre="scope0">${xml1}</foo>""" ≈ <foo xmlns:pre="scope0">{xml2}</foo>)
   }
 }
