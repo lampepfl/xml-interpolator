e1f275c20632c7db224b81aef18f5338b92316af
yibnl <yassin.kammoun@outlook.com>
2019-05-18 14:16:09 +0200

    Add implicit XML top scope

--- src/main/scala/dotty/xml/interpolator/internal/Macro.scala
+++ src/main/scala/dotty/xml/interpolator/internal/Macro.scala
@@ -11,11 +11,10 @@ import scala.language.implicitConversions
 
 object Macro {
 
-  def impl(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[Any]]) given (reflect: Reflection): Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
+  def impl(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[given Scope => Any]], scope: Expr[Scope]) given (reflect: Reflection): Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
     ((strCtxExpr, argsExpr): @unchecked) match {
       case ('{ StringContext(${ExprSeq(parts)}: _*) }, ExprSeq(args)) =>
         val (xmlStr, offsets) = encode(parts)
-        val scope = '{ _root_.scala.xml.TopScope }
         implicit val ctx: XmlContext = new XmlContext(args, scope)
         implicit val reporter: Reporter = new Reporter {
           import reflect._
@@ -34,7 +33,7 @@ object Macro {
         implCore(xmlStr)
     }
   }
-
+/*
   def implErrors(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[Any]]) given (reflect: Reflection): Expr[List[(Int, String)]] = {
     ((strCtxExpr, argsExpr): @unchecked) match {
       case ('{ StringContext(${ExprSeq(parts)}: _*) }, ExprSeq(args)) =>
@@ -59,7 +58,7 @@ object Macro {
         errors.result().toExprOfList
     }
   }
-
+*/
   private def implCore(xmlStr: String) given XmlContext, Reporter, Reflection: Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
 
     import Parse.{apply => parse}
--- src/main/scala/dotty/xml/interpolator/package.scala
+++ src/main/scala/dotty/xml/interpolator/package.scala
@@ -3,8 +3,12 @@ package dotty.xml
 import scala.quoted._
 
 package object interpolator {
+
+  type Scope = scala.xml.NamespaceBinding
+  implicit val top: Scope = scala.xml.TopScope
+
   implicit object StringContextOps {
-    inline def (ctx: => StringContext) xml (args: => Any*) <: Any =
-      ${ dotty.xml.interpolator.internal.Macro.impl('ctx, 'args) }
+    inline def (ctx: => StringContext) xml (args: => (given Scope => Any)*) given Scope <: Any =
+      ${ dotty.xml.interpolator.internal.Macro.impl('ctx, 'args, '{implicitly[Scope]}) }
   }
 }
