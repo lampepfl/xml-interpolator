5af9daf456392e59c4705b8375af0bc4656000fc
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-06-17 14:53:07 +0200

    Upgrade Dotty to 0.25.0-RC2

--- src/main/scala/dotty/xml/interpolator/package.scala
+++ src/main/scala/dotty/xml/interpolator/package.scala
@@ -5,5 +5,5 @@ import scala.quoted._
 type Scope = scala.xml.NamespaceBinding
 implicit val top: Scope = scala.xml.TopScope
 
-inline def (inline ctx: StringContext) xml (inline args: (Scope ?=> Any)*)(using scope: Scope) <: Any =
+transparent inline def (inline ctx: StringContext) xml (inline args: (Scope ?=> Any)*)(using scope: Scope): Any =
   ${ dotty.xml.interpolator.internal.Macro.impl('ctx, 'args, 'scope) }
