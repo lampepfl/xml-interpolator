d3fc10bdc30d22a42f97231de692905c9f809b99
gorilskij <43991400@users.noreply.github.com>
2022-05-11 00:38:57 +0200

    fix some stuff and remove prints

--- src/main/scala/dotty/xml/interpolator/internal/Debug.scala
+++ src/main/scala/dotty/xml/interpolator/internal/Debug.scala
@@ -7,19 +7,45 @@ object Debug {
   def main: Unit = {
     given scope: dotty.xml.interpolator.Scope = new dotty.xml.interpolator.Scope(null, null, null)
 
-    val k = <foo>5</foo>
-    println(k.getClass)
-    val scala.xml.Node(a,b,c) = k
-    println(a.getClass)
-    println(b.getClass)
-    println(c.getClass)
-    return
-
-
 //    val y = xml"<foo>${69}</foo>"
 //    println(y.getClass)
 
-    val xml"<foo>${x}</foo>" = <foo>5</foo>
-    println(x)
+//    val xml"<bar>${x}</bar>" = <bar>5</bar>
+//    println(x)
+//    val xml"<a><b>$a</b><c>$b</c></a>" = <a><b>1</b><c>2</c></a>
+//    println(s"($a, $b)")
+////    val xml"<tag o=\"$j\"></tag>" = <tag o='hmmm'></tag>
+////    println(j)
+
+    // TODO: interpolator doesn't work with attributes (open issue)
+//    val k = xml"<tag o='o' k='0'>0</tag>"
+//    println(k)
+
+    // TODO: match xmlns
+//    val xml"<tag xmlns:attr='a'>$x</tag>" = <tag xmlns:attr='a'>2</tag>
+//    println(x)
+
+    val xml"<tag/>" = <tag/>
+    val xml"<tag/>" = <tag></tag>
+    val xml"<tag></tag>" = <tag/>
+    val xml"<tag></tag>" = <tag></tag>
+
+//    val xml"<t a='v'>$x</t>" = <t a='v'>0</t>
+
+    <hmm a=":)">42</hmm> match
+      case xml"<hmm>$x</hmm>" =>
+        println(s"WRONG, case 1: $x")
+      case xml"<hmm a=''>$x</hmm>" =>
+        println(s"WRONG, case 2: $x")
+      case xml"<hmm a='a'>$x</hmm>" =>
+        println(s"WRONG, case 3: $x")
+      case xml"<hmm a=':) '>$x</hmm>" =>
+        println(s"WRONG, case 4: $x")
+      case xml"<hmm a=':)' b=''>$x</hmm>" =>
+        println(s"WRONG, case 5: $x")
+      case xml"<hmm a=':)'>$x</hmm>" =>
+        println(s"CORRECT, case 6: $x")
+      case _ =>
+        println("WRONG, no case")
   }
 }
--- src/main/scala/dotty/xml/interpolator/internal/Macro.scala
+++ src/main/scala/dotty/xml/interpolator/internal/Macro.scala
@@ -11,19 +11,12 @@ import scala.xml.Node.unapplySeq
 object Macro {
 
   def impl(xmlStrCtxExpr: Expr[XML.StringContext], argsExpr: Expr[Seq[Scope ?=> Any]], scope: Expr[Scope])(using qctx: Quotes): Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
-    println("=============================================================")
-
     val '{ xml(StringContext(${Varargs(parts)}: _*)) } = xmlStrCtxExpr
     val Varargs(args) = argsExpr
     val (xmlStr, offsets) = encode(parts)
 
-    println(s"parts: $parts")
-    println(s"args: $args")
-    println(s"xmlStr: $xmlStr")
-    println(s"offsets: ${java.util.Arrays.toString(offsets)}")
-
-    implicit val ctx: XmlContext = new XmlContext(args, scope)
-    implicit val reporter: Reporter = new Reporter {
+    given XmlContext = new XmlContext(args, scope)
+    given Reporter = new Reporter {
       import quotes.reflect._
 
       def error(msg: String, idx: Int): Unit = {
@@ -40,71 +33,11 @@ object Macro {
     implCore(xmlStr)
   }
 
-//  def compareNodes(skeleton: Tree.Node, data: scala.xml.Node): Boolean =
-//    (skeleton, data) match
-//      case ()
-//
-//  @tailrec
-//  def compareAttrs(skeleton: Seq[Tree.Attribute], data: scala.xml.MetaData): Boolean =
-//    (skeleton, data) match
-//      case (seq@Seq(_, _*), scala.xml.PrefixedAttribute(pre, key, value, next)) =>
-//        val idx = seq.indexWhere { attr =>
-//          attr.pre == pre && attr.key == key && compareNodes(attr.value, value)
-//        }
-//        if idx == -1 then
-//          false
-//        else
-//          val newSeq = seq.take(idx) ++ seq.drop(idx + 1)
-//          compareAttrs(newSeq, next)
-//      case (seq@Seq(_, _*), scala.xml.UnprefixedAttribute(key, value, next)) =>
-//        val idx = seq.indexWhere { attr =>
-//          attr.key == key && compareNodes(attr.value, value)
-//        }
-//        if idx == -1 then
-//          false
-//        else
-//          val newSeq = seq.take(idx) ++ seq.drop(idx + 1)
-//          compareAttrs(newSeq, next)
-//
-//  // TODO: make tail-recursive
-//  def tandemTraverse(skeleton: Tree.Node, data: scala.xml.Node): Seq[Any] = {
-////    def attrsToSeq(metadata: scala.xml.MetaData): Seq[Tree.Attribute] =
-////      metadata match
-////        case scala.xml.Null =>
-////          Nil
-////        case scala.xml.PrefixedAttribute(pre, key, value1, next) =>
-////          val value = value1.map()
-////          attrsToSeq(next).prepended(Tree.Attribute(s"$pre:$key", value))
-////        case scala.xml.UnprefixedAttribute(key, value, next) =>
-////          val Tree.Text(valueStr) = value
-////          attrsToSeq(next).prepended(Tree.Attribute(key, valueStr))
-//
-//    val scala.xml.Node(dataLabel, dataAttrs1, dataChildren) = data
-//    val dataAttrs = attrsToSeq(dataAttrs1)
-//
-//    (skeleton, data) match {
-//      case (Tree.Elem(name, attrs1, children1, _), _: scala.xml.Elem) =>
-//        if name == label && attrs1 == attrs2 || attrs1.isEmpty && attrs2 == null then
-//          children1
-//            .zip(children2)
-//            .flatMap { case (s, d) => tandemTraverse(s, d) }
-//        else
-//          println(s"$name ==? $label, $attrs1 ==? $attrs2")
-//          println(s"${attrs1.getClass} ${attrs2.getClass}")
-//          ???
-//
-//      case (Tree.Placeholder(id), node) =>
-//        Seq(node)
-//    }
-//  }
-
   def implUnapply(xmlStrCtxExpr: Expr[XML.StringContext], elemExpr: Expr[scala.xml.Node | scala.xml.NodeBuffer], scope: Expr[Scope])(using qctx: Quotes): Expr[Option[Seq[Any]]] = {
     val '{ xml(StringContext(${Varargs(parts)}: _*)) } = xmlStrCtxExpr
     val (xmlStr, offsets) = encode(parts)
 
-//    def cf(using Scope) = ???
-//    implicit val ctx: XmlContext = new XmlContext(Seq(), scope)
-    implicit val reporter: Reporter = new Reporter {
+    given Reporter = new Reporter {
       import quotes.reflect._
 
       def error(msg: String, idx: Int): Unit = {
@@ -123,26 +56,15 @@ object Macro {
       import Parse.{apply => parse}
       import Transform.{apply => transform}
       import Validate.{apply => validate}
-      import TypeCheck.{apply => typecheck}
-      import Expand.{apply => expand}
 
-      println("================")
-      println(xmlStr)
+      val process = (
+        parse
+          andThen transform
+          andThen validate
+      )
 
-      val parsed = parse(xmlStr)
-      println("- parse")
-      println(parsed)
-
-      parsed
-
-      // assert single element
-//      val Seq(p) = parsed
-
-//      given XmlContext = new XmlContext(, scope = ???)
-
-//      expand(parsed)
+      process(xmlStr)
     }
-//      expand(parsed)
 
     import scala.quoted.ToExpr.SeqToExpr
 
@@ -155,10 +77,24 @@ object Macro {
     given ToExpr[Tree.Node] with
       def apply(node: Tree.Node)(using Quotes): Expr[Tree.Node] =
         node match {
+          case Tree.Group(nodes) =>
+            '{ Tree.Group(${ Expr(nodes) }) }
           case Tree.Elem(name, attrs, children, end) =>
             '{ Tree.Elem(${ Expr(name) }, ${ Expr(attrs) }, ${ Expr(children) }, ${ Expr(end) }) }
+          case Tree.Text(text) =>
+            '{ Tree.Text(${ Expr(text) }) }
+          case Tree.Comment(text) =>
+            '{ Tree.Comment(${ Expr(text) }) }
           case Tree.Placeholder(id) =>
             '{ Tree.Placeholder(${ Expr(id) }) }
+          case Tree.PCData(data) =>
+            '{ Tree.PCData(${ Expr(data) }) }
+          case Tree.ProcInstr(target, proctext) =>
+            '{ Tree.ProcInstr(${ Expr(target) }, ${ Expr(proctext) }) }
+          case Tree.EntityRef(name) =>
+            '{ Tree.EntityRef(${ Expr(name) }) }
+          case Tree.Unparsed(data) =>
+            '{ Tree.Unparsed(${ Expr(data) }) }
         }
 
     import FillPlaceholders.{apply => fill_placeholders}
@@ -166,35 +102,20 @@ object Macro {
     '{
       ${elemExpr} match {
         case e: scala.xml.Node =>
-//          given Quotes = ${ Expr(qctx) }
-//          val q = summon[Quotes]
           for {
-            map <- fill_placeholders(${ Expr(parsed) }, Seq(e))//(${ Expr(qctx) })
+            map <- fill_placeholders(${ Expr(parsed) }, Seq(e))
           } yield {
             val keys = map.keys.toList.sorted
-            assert(!keys.zipWithIndex.exists((k, i) => k == i))
+            assert(!keys.zipWithIndex.exists((k, i) => k != i))
             keys.map(map)
           }
         case e =>
+          // TODO: can this be NodeBuffer?
           println("???")
           println(e.getClass)
           ???
       }
     }
-
-//    '{
-//      ${elemExpr} match {
-//        case e: Tree.Node => Some(${Expr(tandemTraverse(parsed, e))})
-//        case _ => ???
-//      }
-
-//      ${implCore(xmlStr)} match {
-//        case scala.xml.Node(label, attrs, child) => Some(Seq(label, attrs, child))
-//        case _ => ???
-////        case scala.xml.NodeBuffer(_*) => ???
-//      }
-//    }
-//    ???
   }
 
   def implErrors(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[Scope ?=> Any]], scope: Expr[Scope])(using qctx: Quotes): Expr[List[(Int, String)]] = {
@@ -223,42 +144,21 @@ object Macro {
   }
 
   private def implCore(xmlStr: String)(using XmlContext, Reporter, Quotes): Expr[scala.xml.Node | scala.xml.NodeBuffer] = {
-
     import Parse.{apply => parse}
     import Transform.{apply => transform}
     import Validate.{apply => validate}
     import TypeCheck.{apply => typecheck}
     import Expand.{apply => expand}
 
-//    val interpolate = (
-//      parse
-//        andThen transform
-//        andThen validate
-//        andThen typecheck
-//        andThen expand
-//    )
-//
-//    interpolate(xmlStr)
+    val interpolate = (
+      parse
+        andThen transform
+        andThen validate
+        andThen typecheck
+        andThen expand
+    )
 
-    val x = xmlStr
-    println("  == implCore ==")
-    println(x)
-    println("> parse <")
-    var x1 = parse(x)
-    println(x1)
-    println("> transform <")
-    x1 = transform(x1)
-    println(x1)
-    println("> validate <")
-    x1 = validate(x1)
-    println(x1)
-    println("> typecheck <")
-    x1 = typecheck(x1)
-    println(x1)
-    println("> expand <")
-    val x2 = expand(x1)
-    println(x2.show)
-    x2
+    interpolate(xmlStr)
   }
 
   private def encode(parts: Seq[Expr[String]])(using Quotes): (String, Array[Int]) = {
@@ -267,7 +167,7 @@ object Macro {
 
     def appendPart(part: Expr[String]) = {
       bf += sb.length
-      sb ++= part.valueOrError
+      sb ++= part.valueOrAbort
       bf += sb.length
     }
 
